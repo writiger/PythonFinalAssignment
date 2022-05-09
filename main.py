850c07638d20b90af606f3ffe749b40dfe6b79ee
writiger <75297838+writiger@users.noreply.github.com>
2022-05-09 10:23:42 +0800

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.modules.read_json import read_json
+
+from src.modules.visualization import vis
+
+if __name__ == '__main__':
+    # myDict = read_json('resource/socrata_metadata_nndss-table-ii.-mumps-to-rabies-animal.json')
+    # print(myDict)
+    vis()
+
