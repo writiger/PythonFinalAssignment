69774f4aaa7e7fac7770d0d323a8ce32b80e45b4
writiger <75297838+writiger@users.noreply.github.com>
2022-05-16 10:36:32 +0800

    调整资源文件位置

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.modules.analysis_package import analysis_package
 from src.modules.visualization import visualize
 
 if __name__ == '__main__':
-    dataSet = load_csv('resource/nndss-table-ii.-mumps-to-rabies-animal.csv')  # 从csv中读取数据
+    dataSet = load_csv('../resource/nndss-table-ii.-mumps-to-rabies-animal.csv')  # 从csv中读取数据
     anaResult = analysis_package(dataSet)  # 分析数据并打包
     visualize(anaResult)  # 数据可视化
 
