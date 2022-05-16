fb62e93a5bb1e6301765970902e18ee9fd070553
writiger <75297838+writiger@users.noreply.github.com>
2022-05-16 10:33:51 +0800

    优化更新图标代码格式

--- src/modules/options/update_pic.py
+++ src/modules/options/update_pic.py
@@ -3,18 +3,51 @@ from src.modules.options.week_vis import show_histogram
 from src.modules.tools.specification import divider, interval
 
 
-def update_pic(data):
+def update_week(data):
+    """ 更新或加载按星期分类的图表
+
+    :param data: 数据集
+    :return: None
+    """
     maxWeek = len(data.tables)
     updateNow = 0
     for i in range(maxWeek):
         updateNow += 1
-        divider("当前更新至第%d个图片" % updateNow)
+        divider("当前更新至第%d个按星期分类图片" % updateNow)
         show_histogram(str(i + 1), data.tables[i], False)
+
+
+def update_region(data):
+    """ 更新或加载按地区分类的图表
+
+    :param data: 数据集
+    :return: None
+    """
     regionCnt = len(data.tables[0].x)
+    updateNow = 0
     for i in range(regionCnt):
         updateNow += 1
-        divider("当前更新至第%d个图片" % updateNow)
+        divider("当前更新至第%d个按地区分类图片" % updateNow)
         show_line_chart(data.tables[0].x[i], find_region(data, i), False)
-    divider("当前更新至第%d个图片" % (updateNow + 1))
+
+
+def update_pie(data):
+    """ 更新或加载按地区饼图
+
+    :param data: 数据集
+    :return: None
+    """
+    divider("当前更新地区饼图")
     show_pie_chart(data, False)
+
+
+def update_pic(data):
+    """ 更新或加载所有图表
+
+    :param data: 数据集
+    :return: None
+    """
+    update_week(data)
+    update_region(data)
+    update_pie(data)
     divider("更新完成")
